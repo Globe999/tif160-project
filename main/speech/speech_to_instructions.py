226403fdaaab0f79071f7199d046445a62e332b3
tryggebygge <oskartryggvason@gmail.com>
2024-10-01 13:45:36 +0200

    implemented functions for mode and order

--- main/speech/speech_to_instructions.py
+++ main/speech/speech_to_instructions.py
@@ -20,44 +20,96 @@ def get_text():
     print(words)
     return words
 
+def get_mode(modes):
+    
+    
+    engine = pyttsx3.init()
+    # Define the text you want to convert to speech
+    text = "What mode should i sort by"
+    # Convert the text to speech
+    engine.say(text)
+
+    # # Run the speech engine
+    engine.runAndWait()
+    
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=1)
+    while True:
+        with mic as source:
+            print("Speak Anything!")
+            audio = r.listen(source,timeout=2,phrase_time_limit=10)
+        #load in audiofile
+
+        #use googles text to speech to get text
+        text = r.recognize_google(audio,show_all=True)['alternative'][0]
+        words = text['transcript'].lower().split()
+    
+        for mode in modes:
+            if mode in words:
+                text = "Ok, i will sort by " + mode
+        # Convert the text to speech
+                engine.say(text)
+
+                # # Run the speech engine
+                engine.runAndWait()
+                return mode
+        
+        text = "Please repeat what mode you want"
+        # Convert the text to speech
+        engine.say(text)
+
+        # # Run the speech engine
+        engine.runAndWait()
 
 
-def get_command(words):
+def get_command(mode):
     
     shapes = ["cube","star","hexagon"]
     colors = ["red","green","blue"]
     size = ["big","small"]
-    modes = ["shape","size","color"]
-    
     instructions = []
-    mode = "NONE"
-    for word in words:
-        
-        # if(word in colors):
-        #     instruction = []
-        #     instruction.append(word)
-        # elif(word in shapes):
-        #     instruction.append(word)
-        #     instructions.append(instruction)
-        if word in modes:
-            mode = word
-        if mode == "shape" and word in shapes:
-            instructions.append(word)
-        elif mode == "size" and word in size:
-            instructions.append(word)
-        elif mode == "color" and word in colors:
-            instructions.append(word)
+    
+    engine = pyttsx3.init()
+    # Define the text you want to convert to speech
+    text = "In what order shall i sort by " + mode
+    # Convert the text to speech
+    engine.say(text)
+
+    # # Run the speech engine
+    engine.runAndWait()
+    
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=1)
+    while True:
+        with mic as source:
+            print("Speak Anything!")
+            audio = r.listen(source,timeout=2,phrase_time_limit=10)
             
-    if len(instructions) < 2 or mode =="NONE":
-        print("Please repeat your instructions")
-        return 0,[]
-    else:
-        print("mode is set to: " + mode.upper())
-        print("Order to sort in: ")
-        for instruction in instructions:
-            print(instruction.upper())
+        text = r.recognize_google(audio,show_all=True)['alternative'][0]
+        words = text['transcript'].lower().split()
+        
+        for word in words:
+            if mode == "shape" and word in shapes:
+                instructions.append(word)
+            elif mode == "size" and word in size:
+                instructions.append(word)
+            elif mode == "color" and word in colors:
+                instructions.append(word)
                 
-        return mode, instructions
+        if len(instructions):
+            text = "Order set to "+ ", ".join(instructions) 
+        
+            engine.say(text)
+            engine.runAndWait()
+            return instructions
+        
+        text = "Please repeat what order you want"
+        # Convert the text to speech
+        engine.say(text)
+
+        # # Run the speech engine
+        engine.runAndWait()
+
     
 def get_instructions():
     words = get_text()     
@@ -74,3 +126,7 @@ def get_instructions():
     return mode, order
 
 
+mode = get_mode(["shape","size","color"])
+instructions = get_command(mode)
+print(mode)
+print(instructions)
