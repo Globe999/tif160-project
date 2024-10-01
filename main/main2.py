720c2edaf75d8bedbe8a4c44c169b39b9c0f6c16
Globee <marten.sanderod@gmail.com>
2024-10-01 14:11:00 +0200

    Change few things

--- main/main2.py
+++ main/main2.py
@@ -1,4 +1,6 @@
 import random
+from typing import List
+from main.speech.speech_to_instructions import AudioInterface
 from main.vision.vision import CameraDetection
 
 
@@ -9,11 +11,13 @@ def get_sorted_objects(sort_mode, order, objects):
         objects, key=lambda x: rank.get(getattr(x, sort_mode), float("inf"))
     )
 
+    # Remove all objects not in order
+    sorted_objects = [obj for obj in sorted_objects if getattr(obj, sort_mode) in order]
+
     return sorted_objects
 
 
-def main():
-
+def mock_get_objects() -> List[CameraDetection]:
     # Mock data generation
     types = ["circle", "square", "triangle"]
     colors = ["red", "green", "blue"]
@@ -33,15 +37,33 @@ def main():
         )
         mock_data.append(detection)
 
-    sort_mode = "color"
-    order = ["red", "green", "blue"]
+    return mock_data
 
-    new_list = get_sorted_objects(sort_mode, order, mock_data)
+
+def main():
+
+    mock_data = mock_get_objects()
+
+    available_sort_modes = ["shape", "color", "size"]
+
+    available_colors = list({x.color for x in mock_data})
+    available_shapes = list({x.shape for x in mock_data})
+    available_sizes = list({x.size for x in mock_data})
+
+    audio_interface = AudioInterface()
+
+    mode = audio_interface.get_mode(available_sort_modes)
+
+    order = audio_interface.get_instructions()
+
+    # sort_mode = "color"
+    # order = ["red", "green"]
+
+    new_list = get_sorted_objects(mode, order, mock_data)
     for i, (old, new) in enumerate(zip(mock_data, new_list)):
-        print(f"{i}: {getattr(old,sort_mode)} -> {getattr(new,sort_mode)}")
+        print(f"{i}: {getattr(old,mode)} -> {getattr(new,mode)}")
 
     # sort_mode, order = get_instructions()
-
     # objects = camera.get_objects()
 
 
--- main/speech/speech_to_instructions.py
+++ main/speech/speech_to_instructions.py
@@ -1,63 +1,62 @@
 import speech_recognition as sr
 import pyttsx3
+
 # for index, name in enumerate(sr.Microphone.list_microphone_names()):
 #     print(f'{index}, {name}')
 
-class AudioInterface():
+
+class AudioInterface:
     def __init__(self) -> None:
-        self.mic = sr.Microphone(device_index=1)
+        self.mic = sr.Microphone(device_index=0)
         self.r = sr.Recognizer()
         self.engine = pyttsx3.init()
         self.mode = ""
         self.instructions = []
-        
+
     def take_audio_input(self):
-        
+
         with self.mic as source:
             print("Speak Anything!")
-            audio = self.r.listen(source,timeout=2,phrase_time_limit=10)
-        #load in audiofile
+            audio = self.r.listen(source, timeout=2, phrase_time_limit=10)
+        # load in audiofile
 
-        #use googles text to speech to get text
-        text = self.r.recognize_google(audio,show_all=True)['alternative'][0]
-        words = text['transcript'].lower().split()
+        # use googles text to speech to get text
+        text = self.r.recognize_google(audio, show_all=True)["alternative"][0]
+        words = text["transcript"].lower().split()
         return words
-    
-    def output_audio(self,string):
+
+    def output_audio(self, string):
         self.engine.say(string)
         self.engine.runAndWait()
-        
-    def get_mode(self,modes):
-        
+
+    def get_mode(self, modes):
+
         self.output_audio("What mode should i sort by")
-        
+
         while True:
             words = self.take_audio_input()
-        
+
             for mode in modes:
                 if mode in words:
                     self.output_audio("Ok, i will sort by " + mode)
                     return mode
-                
+
             self.output_audio("Please repeat what mode you want")
 
+    def get_command(self, mode):
 
-    def get_command(self,mode):
-        
-        shapes = ["cube","star","hexagon"]
-        colors = ["red","green","blue"]
-        size = ["big","small"]
+        shapes = ["cube", "star", "hexagon"]
+        colors = ["red", "green", "blue"]
+        size = ["big", "small"]
         instructions = []
-        
-        
+
         # Define the text you want to convert to speech
         # Convert the text to speech
         self.output_audio("In what order shall i sort by " + mode)
-        
-        
+
         while True:
             words = self.take_audio_input()
-            
+
             for word in words:
                 if mode == "shape" and word in shapes:
                     instructions.append(word)
@@ -65,21 +64,19 @@ class AudioInterface():
                     instructions.append(word)
                 elif mode == "color" and word in colors:
                     instructions.append(word)
-                    
+
             if len(instructions):
-                text = "Order set to "+ ", ".join(instructions) 
-            
+                text = "Order set to " + ", ".join(instructions)
+
                 self.output_audio(text)
                 return instructions
-            
+
             self.output_audio("Please repeat what order you want")
 
-        
     def get_instructions(self):
-        words = self.take_audio_input()     
+        words = self.take_audio_input()
         mode, order = self.get_command(words)
-        
-        
+
         # Define the text you want to convert to speech
         text = "Ofcourse, i will sort by " + mode
         # Convert the text to speech
