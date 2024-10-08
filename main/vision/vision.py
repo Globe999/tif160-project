f7423970f3073ddaa92ca8feca3e58cf1e245309
tryggebygge <oskartryggvason@gmail.com>
2024-10-08 16:28:40 +0200

    removed useless function

--- main/vision/vision.py
+++ main/vision/vision.py
@@ -170,8 +170,6 @@ class Camera:
             )
 
         return objects
-    def calc_distance(point1, point2):
-        return np.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
     
     def merge_objects(self,objects:List[CameraDetection],threshold):
         
